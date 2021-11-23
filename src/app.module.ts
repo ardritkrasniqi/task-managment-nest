94bba9d0c20da8a5d09c974f8eddbb49fd370c3d
ardritkrasniqi <ardritdev@gmail.com>
2021-11-23 23:14:27 +0100

    typeorm and database connectin with pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,18 @@
 /*
  * @Author: Ardrit Krasniqi 
  * @Date: 2021-10-16 23:44:21 
- * @Last Modified by:   Ardrit Krasniqi 
- * @Last Modified time: 2021-10-16 23:44:21 
+ * @Last Modified by: Ardrit Krasniqi ©
+ * @Last Modified time: 2021-11-23 23:12:17
  */
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
-    TasksModule
+    TasksModule,
+    TypeOrmModule.forRoot(typeOrmConfig)
   ]
 })
 export class AppModule {}
