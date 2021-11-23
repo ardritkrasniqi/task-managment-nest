783cbc7f8a8905802189313291730b3a9385a378
ardritkrasniqi <ardritdev@gmail.com>
2021-11-23 23:30:30 +0100

    added the task entity and the task (entity) columns

---
+++ src/tasks/task.entity.ts
@@ -0,0 +1,22 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { TaskStatus } from "./task.model";
+
+@Entity()
+export class Task extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+    
+    @Column("varchar", { length: 255 , nullable: false})
+    title: string;
+
+    @Column("text", { nullable: false })
+    description: string;
+
+    @Column({
+        type: "enum",
+        enum: TaskStatus,
+        default: TaskStatus.OPEN,
+        nullable: false
+    })
+    status: TaskStatus;
+}
