8cc33eee92d96885849ff56c3823f6a9b12e4deb
ardritkrasniqi <ardritdev@gmail.com>
2021-11-25 23:27:30 +0100

    added the repository functions to fetch data

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -3,5 +3,14 @@ import { EntityRepository, Repository } from "typeorm";
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
-    
+    public async getTaskById(id: number): Promise<Task> {
+        const task = await this.findOne(id);
+        return task;
+    }
+
+
+    public async getTasks(): Promise<Task[]> {  
+        const tasks = await this.find();
+        return tasks;
+    }
 }
