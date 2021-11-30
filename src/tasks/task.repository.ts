550067e43a1148bacf124ec5d2df822e82098ed7
ardritkrasniqi <ardritdev@gmail.com>
2021-11-30 23:25:50 +0100

    added all task functions in repositroy to fetch from the db

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -19,4 +19,23 @@ export class TaskRepository extends Repository<Task> {
         const newTask = await this.save(task);
         return newTask;
     }
+
+    public async updateTask(id: number, task: Task): Promise<Task> {
+        const updatedTask = await this.save(task);
+        return updatedTask;
+    }
+
+    public async deleteTask(id: number): Promise<void> {
+        await this.delete(id);
+    }
+
+    public async getTasksByUserId(userId: number): Promise<Task[]> {
+        const tasks = await this.find({ where: { userId: userId } });
+        return tasks;
+    }
+
+    public async getTasksByUserIdAndStatus(userId: number, status: string): Promise<Task[]> {
+        const tasks = await this.find({ where: { userId: userId, status: status } });
+        return tasks;
+    }
 }
