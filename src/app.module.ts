f8c5a9ccd6da72f40e6a981cebda49bcc4cdfaf4
ardritkrasniqi <ardritdev@gmail.com>
2021-12-08 23:20:56 +0100

    changes in module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,20 @@
  * @Author: Ardrit Krasniqi 
  * @Date: 2021-10-16 23:44:21 
  * @Last Modified by: Ardrit Krasniqi ©
- * @Last Modified time: 2021-12-03 15:28:18
+ * @Last Modified time: 2021-12-08 17:36:36
  */
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     TasksModule,
     TypeOrmModule.forRoot(typeOrmConfig),
     ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true,
+      isGlobal: true
     })
   ]
 })
