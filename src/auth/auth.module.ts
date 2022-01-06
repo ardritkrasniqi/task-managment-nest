f893c7b512708d9d8563f4a3132beffaf04c374a
ardritkrasniqi <ardritdev@gmail.com>
2022-01-06 23:48:51 +0100

    added some comments and features

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,3 +1,9 @@
+/*
+ * @Author: Ardrit Krasniqi © 
+ * @Date: 2022-01-06 23:48:19 
+ * @Last Modified by:   Ardrit Krasniqi © 
+ * @Last Modified time: 2022-01-06 23:48:19 
+ */
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
